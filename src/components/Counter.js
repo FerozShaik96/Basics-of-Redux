8e6f06332cf2a9d90ecf38387bc24e2285569471
FerozShaik96 <ferozshaik1997@gmail.com>
2023-07-26 17:28:01 +0530

    Provided the store to react App and using useSelector and use Dispatch hooks to dispatch actions and subscribe to the components

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,27 @@
 import classes from "./Counter.module.css";
+import { useSelector, useDispatch } from "react-redux";
 const Counter = () => {
+  const dispatch = useDispatch();
+
+  const counter = useSelector((state) => state.counter);
   const toggleCounterHandler = () => {};
+  const IncrementHandler = () => {
+    dispatch({ type: "increment", payload: 5 });
+  };
+
+  const DecrementHandler = () => {
+    dispatch({ type: "decrement", payload: 5 });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>--- Counter ---</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={IncrementHandler}>Increment</button>
 
+        <button onClick={DecrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
