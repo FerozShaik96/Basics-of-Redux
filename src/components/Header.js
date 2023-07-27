e18c6184d389b383ca775e3153e1025bd25087e8
FerozShaik96 <ferozshaik1997@gmail.com>
2023-07-27 13:20:24 +0530

    Added Login and logout functionality and dispatched actions to the store

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,29 @@
-import classes from './Header.module.css';
-
+import classes from "./Header.module.css";
+import { authActions } from "../Store/Store";
+import {  useDispatch, useSelector } from "react-redux";
 const Header = () => {
+  const dispatch = useDispatch();
+  const showLoggin = useSelector((state) => state.Auth.isAuth);
+  const logoutHandler = () => {
+    dispatch(authActions.isLogout());
+  };
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
-        <ul>
-          <li>
-            <a href='/'>My Products</a>
-          </li>
-          <li>
-            <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
+        {showLoggin && (
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={logoutHandler}>Logout</button>
+            </li>
+          </ul>
+        )}
       </nav>
     </header>
   );
