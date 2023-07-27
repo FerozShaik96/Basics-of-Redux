66b1df8f9d28c470589be6b014b8d8f9d660ff01
FerozShaik96 <ferozshaik1997@gmail.com>
2023-07-27 16:14:41 +0530

    Rendering the Components based on if user is logged in or not

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Counter from "./components/Counter";
 import Header from "./components/Header";
 import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux/es/hooks/useSelector";
 function App() {
+  const showCounter = useSelector((state) => state.Auth.isAuth);
   return (
     <>
       <Header />
-      <Auth />
-      <Counter />
+      {!showCounter && <Auth />}
+      {showCounter && <UserProfile />}
+      {showCounter && <Counter />}
     </>
   );
 }
